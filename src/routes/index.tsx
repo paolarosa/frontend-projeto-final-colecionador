f4fec0a40104ad4af2e6f61455e30f187bab75cb
eliasjp <illumine1998@gmail.com>
2023-01-04 11:36:08 -0300

    create homepage template

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginRigisterProvider } from "../contexts/contexLoginRegister";
 import Login from "../pages/Login";
+import Home from "../pages/Home"
 
 export const RoutesMain: any = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route element={<LoginRigisterProvider />}>
         <Route path="/login" element={<Login />} />
-        
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
