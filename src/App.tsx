1027cd0d0ab225861fdc99a6b6cf33e2a8adb160
Nicolas <109254154+kZNick@users.noreply.github.com>
2023-01-09 20:30:01 -0300

    Fix: fixing toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 import { RoutesMain } from "./routes";
-import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <>
       <RoutesMain />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      <ToastContainer />
     </>
   );
 }
