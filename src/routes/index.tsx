57a6052a5376c624d6e99bf6d8fe5342d605fdab
eliasjp <illumine1998@gmail.com>
2023-01-11 22:23:22 -0300

    remove billboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { LoginRigisterProvider } from "../contexts/contexLoginRegister";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import Register from "../pages/Register";
-import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import Forum from "../pages/Forum";
 import Admin from "../pages/Admin/AdmPage";
@@ -16,9 +15,8 @@ export const RoutesMain: any = () => {
       <Route element={<LoginRigisterProvider />}>
         <Route element={<AuthProvider/>}>
           <Route element={<ForumProvider />}>
-            <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="*" element={<Navigate to="/" />} />
             <Route path="/profile" element={<Profile />} />
